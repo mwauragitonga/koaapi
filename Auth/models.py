4bc3b6e38d0a9de1b7a07a4bd6f56789f1b3b079
gitongamwaura <mwauragitonga12@gmail.com>
2023-05-18 17:22:18 +0300

    update readme

--- Auth/models.py
+++ Auth/models.py
@@ -1,9 +1,1 @@
-from django.db import models
-
-class User(models.Model):
-    username = models.CharField(max_length=200, null=True)
-    password = models.CharField(max_length=200, null=True)
-    last_login = models.DateTimeField(auto_now_add=True, null=True)
-    date_joined = models.DateTimeField(auto_now=True, null=True)
-    def __str__(self):
-       return str(self.name)
+# 
